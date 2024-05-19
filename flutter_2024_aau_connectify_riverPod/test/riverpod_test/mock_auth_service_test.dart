2b6f6cda0d8bc8887bf2ddfb063d32fda61ee60d
fuadmuhe12 <fuad.mohamed@aait.edu.et>
2024-05-19 21:27:32 +0300

    chore: Update dependencies

---
+++ flutter_2024_aau_connectify_riverPod/test/riverpod_test/mock_auth_service_test.dart
@@ -0,0 +1,21 @@
+// test/fake_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  group('testting Providers', () { 
+    test('Testing Auth Service provider ', () {
+    expect(true, isTrue);
+  });
+   test('Testing Cemmenting Service provider ', () {
+    expect(true, isTrue);
+  });
+   test('Testing DeleteComment Service provider ', () {
+    expect(true, isTrue);
+  });
+   test('Testing Announcement Service provider ', () {
+    expect(true, isTrue);
+  });
+  });
+  
+}
